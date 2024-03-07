302091230841ebe202b99a5c264f5378321c83c8
reshabh27 <reshabhsahu10@gmail.com>
2024-03-07 17:18:14 +0530

    Connected NodeJs app to mysql server through sequelize

--- db.js/index.js
+++ db.js/index.js
@@ -1,2 +1,23 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
+const sequelize = new Sequelize('logistic-management', 'root', '', {
+    host: "127.0.0.1",
+    port: '3306',
+    dialect: 'mysql'
+});
+
+
+sequelize.authenticate().then(() => {
+    console.log('Connected to database');
+}).catch((e) => {
+    console.log(e);
+})
+
+sequelize.sync().then(() => {
+
+}).catch((e) => {
+    console.log("there is an error while synching", e);
+})
+
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('./db.js');
 require('dotenv').config()
 
 
