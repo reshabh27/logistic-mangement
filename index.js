ecd857b201a815231848eadfe161869857afd9ad
DeepaliJangid <151712176+DeepaliJangid@users.noreply.github.com>
2024-03-08 16:01:34 +0530

    env
    environment file created.

--- index.js
+++ index.js
@@ -2,16 +2,7 @@ const express = require('express');
 require('./db.js');
 require('dotenv').config()
 
-
 const app = express();
-
-
-
-
-
-
-
-
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`server started at ${process.env.SERVER_PORT}`);
