5fd5bf12bfb30952892562faae208b80c757d5c8
DeepaliJangid <151712176+DeepaliJangid@users.noreply.github.com>
2024-03-08 15:57:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const express = require('express');
+const cors = require('cors')
 require('./db.js');
 require('dotenv').config()
 
 
 const app = express();
-
-
-
-
-
-
-
-
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`server started at ${process.env.SERVER_PORT}`);
