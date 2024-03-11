b393af358f7b00ec3c6123cf487faf8f7230644a
Jayesh Chauhan <jayesh.chauhan@cubastion.com>
2024-03-11 18:27:02 +0530

    adding relationships in product management

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
-const express = require('express');
-require('dotenv').config()
+const express = require("express");
+require("dotenv").config();
 
-const cors = require('cors');
+const cors = require("cors");
 
-
-
-const orderRouter = require('./routes/orderRoutes.js');
-const userRoute = require('./routes/userRoutes.js')
+const orderRouter = require("./routes/orderRoutes.js");
+const userRoute = require("./routes/userRoutes.js");
 const { productRouter } = require("./routes/ProductRoute.js");
-const { Product } = require('./models/products.js');
+const db = require("./db");
 
-
-require('./db');
+require("./db");
 
 const app = express();
 
-Product.sync().then(() => { console.log("resync product model") }).catch((err) => { console.log(err) });
+db.products.sync()
+  .then(() => {
+    console.log("resync product model");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+db.productSupplier
+  .sync()
+  .then(() => {
+    console.log("resync productSupplier model");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(express.json());
 
-app.use('/orders',orderRouter)
-
- 
 
 
 
+app.use("/orders", orderRouter);
 
 app.use("/user", userRoute);
 app.use("/products", productRouter);
 
-
-
-
-
 app.listen(process.env.SERVER_PORT, () => {
-    console.log(`server started at ${process.env.SERVER_PORT}`);
-})
+  console.log(`server started at ${process.env.SERVER_PORT}`);
+});
