caade6a5e032834b32d4f9f89d6738455173ef88
Jayesh Chauhan <jayesh.chauhan@cubastion.com>
2024-03-12 18:20:08 +0530

    added more crud route handlers for inventory model

--- models/inventory.js
+++ models/inventory.js
@@ -1,49 +1,54 @@
-module.exports = (sequelize,DataTypes) => {
-    const Inventory = sequelize.define("Inventory",{
-        wareHouseId: {
-            type: DataTypes.INTEGER,
-            allowNull:false,
-            validate:{
-                notNull:true,
-                notEmpty:true
+module.exports = (sequelize, DataTypes) => {
+  const Inventory = sequelize.define("Inventory", {
+    productId: {
+        type: DataTypes.INTEGER,
+        allowNull:false,
+        validate:{
+            notNull:true,
+            notEmpty:true
+        }
+      },
+      wareHouseId: {
+        type: DataTypes.INTEGER,
+        allowNull:false,
+        validate:{
+            notNull:true,
+            notEmpty:true
+        }
+      },
+      reorderLevel: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          isFloat: true,
+          notNull: true,
+          isValid(value) {
+            if (value < 0) {
+              throw new Error(
+                "Reorder Level  cannot be a negative number."
+              );
             }
           },
-          productId: {
-            type: DataTypes.INTEGER,
-            allowNull:false,
-            validate:{
-                notNull:true,
-                notEmpty:true
-            }
-          },
-          quantity: {
-            type: DataTypes.INTEGER,
-            allowNull:false,
-            validate:{
-                isFloat:true,
-                notNull:true,
-                isValid(value){
-                    if(value<0){
-                        throw new Error("Quantity of a product cannot be a negative number.")
-                    }
-                },
-                min:0
+          min: 0,
+        },
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull:false,
+        validate:{
+            isFloat:true,
+            notNull:true,
+            isValid(value){
+                if(value<0){
+                    throw new Error("Quatity of a product cannot be a negative number.")
+                }
             },
-          },
-          reorderLevel:{
-            type: DataTypes.INTEGER,
-            allowNull:false,
-            validate:{
-                isFloat:true,
-                notNull:true,
-                isValid(value){
-                    if(value<0){
-                        throw new Error("Quantity of a product cannot be a negative number.")
-                    }
-                },
-                min:0
-            },
-          }
-    });
-    return Inventory;
+            min:0
+        },
+      },
+  });
+  return Inventory;
 };
+
+
+
--- models/products.js
+++ models/products.js
@@ -6,7 +6,7 @@ module.exports=(sequelize,DataTypes)=>{
 const Product = sequelize.define("product",{
     name:{
         type:DataTypes.STRING,
-        allowNull:false,
+        allowNull:false, 
         validate:{
             notEmpty:true,
         }
