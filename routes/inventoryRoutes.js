eff81f29484eb29c6f12fc440c7a733ab3d89df9
reshabh27 <reshabhsahu10@gmail.com>
2024-03-14 15:01:37 +0530

    Added restriction based on roles in inventory, transport, warehouse routes

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const { check, addInventory, updateInventory, deleteInventory, specificProductInventory } = require('../controllers/inventoryController');
+const { auth } = require('../middleware/auth');
 
 const inventoryRouter = express.Router();
 
 // route for checking route handlers
-inventoryRouter.get("/check",check);
+inventoryRouter.get("/check", check);
 
 // route for adding inventory
-inventoryRouter.post("/",addInventory);
+inventoryRouter.post("/", auth, addInventory);
 
 // route for updating inventory
-inventoryRouter.patch("/:id",updateInventory);
+inventoryRouter.patch("/:id", auth, updateInventory);
 
 // route for deleting inventory from it's id
-inventoryRouter.delete("/:id",deleteInventory);
+inventoryRouter.delete("/:id", auth, deleteInventory);
 
 // route for retrieving a list of all inventory items for a specific product across all warehouses.
-inventoryRouter.get("/products/:id",specificProductInventory);
+inventoryRouter.get("/products/:id", auth, specificProductInventory);
 
-module.exports = {inventoryRouter};
+module.exports = { inventoryRouter }; 
