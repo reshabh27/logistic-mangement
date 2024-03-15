223703fa7d723ffb16d11992851161761c10a1a8
reshabh27 <reshabhsahu10@gmail.com>
2024-03-15 12:46:43 +0530

    fixed asyncErrorHandler to send correct message in response

--- utils/asyncErrorHandler.js
+++ utils/asyncErrorHandler.js
@@ -3,6 +3,11 @@
 
 module.exports = (func) => {
     return (req, res, next) => {
-        func(req, res).catch(err => next(console.log(err)))
+        func(req, res).catch(err => {
+            // console.log("its from me");
+            // console.log(err);
+            return res.status(400).json({ error: err.message });
+            // next();
+        })
     }
 }
