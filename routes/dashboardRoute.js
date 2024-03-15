c67157231bc0f9dca198cf0f28662aea13cc0719
AshitPalai12 <ashit.palai@cubastion.com>
2024-03-15 12:17:05 +0530

    worked on  transport and order dashboard

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const { auth } = require('../middleware/auth');
-const { handleAdminDashboard } = require('../controllers/dashboardController');
+const express = require("express");
+const { auth } = require("../middleware/auth");
+const {
+  handleAdminDashboard,
+  handleOrderDashboard,
+  handleTransportDashboard,
+} = require("../controllers/dashboardController");
 const router = express.Router();
 
-
-router.get('/', auth, (req, res) => {
-    if (req.role === 'Admin')
-        return handleAdminDashboard(req, res);
-    else
-        return res.send("its dashborad but nothing After auth.")
-})
+router.get("/", auth, (req, res) => {
+  if (req.role === "Admin") return handleAdminDashboard(req, res);
+  else return res.send("its dashborad but nothing After auth.");
+});
+router.get("/orderDashboard", handleOrderDashboard);
+router.get("/transportDashboard", handleTransportDashboard);
 
 module.exports = router;
