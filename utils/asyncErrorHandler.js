f55fb8806f91117e48abd0018e043b6c2d2d52b7
reshabh27 <reshabhsahu10@gmail.com>
2024-03-15 16:39:26 +0530

    changed user model to store login timing also and added method to sent that detail in dashboard

--- utils/asyncErrorHandler.js
+++ utils/asyncErrorHandler.js
@@ -4,10 +4,7 @@
 module.exports = (func) => {
     return (req, res, next) => {
         func(req, res).catch(err => {
-            // console.log("its from me");
-            // console.log(err);
             return res.status(400).json({ error: err.message });
-            // next();
         })
     }
 }
